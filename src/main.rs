eb6c2fc400066de3b2c23bc51fe74999104b8e84
GruffTrick <gruff.trick@gmail.com>
2023-02-18 17:24:08 +0000

    Basic csv reading capability. Using uspop.csv example data for testing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    for result in rdr.records() {
+        let record = result.expect("a csv record");
+
+        println!("{:?}", record)
+    }
+
 }
