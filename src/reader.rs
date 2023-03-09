c851512290d7a877fafb64ac696eb796bb4f3119
GruffTrick <gruff.trick@gmail.com>
2023-03-09 12:16:08 +0000

    rework of get_header functionality

--- src/reader.rs
+++ src/reader.rs
@@ -2,6 +2,7 @@ use std::fs::{File, read};
 use std::io;
 use std::io::Stdin;
 use std::path::Path;
+use std::error::Error;
 use csv::{Reader, StringRecord};
 
 
@@ -9,7 +10,7 @@ pub fn get_reader_stdin() -> Reader<io::Stdin> {
     let mut reader = Reader::from_reader(io::stdin());
     reader
 }
-
+//
 // pub fn get_reader_path(p: &Path) -> Reader<File> {
 //     let mut reader = Reader::from_path(p);
 //     let mut v: Vec<StringRecord> = Vec::new();
@@ -17,8 +18,18 @@ pub fn get_reader_stdin() -> Reader<io::Stdin> {
 //     reader
 // }
 
-pub fn get_records(reader:Reader<Stdin>) -> Vec<StringRecord> {
+
+pub fn get_headers(reader: &mut Reader<Stdin>) -> StringRecord {
     let mut reader = reader;
+
+    let headers = reader.headers().cloned().expect("Panic: Expected headers");
+    println!("{:?}", headers);
+
+    headers
+}
+
+pub fn get_records(reader: &mut Reader<Stdin>) -> Vec<StringRecord> {
+    let mut reader= reader;
     let mut records: Vec<StringRecord> = Vec::new();
 
     for result in reader.records() {
@@ -31,11 +42,3 @@ pub fn get_records(reader:Reader<Stdin>) -> Vec<StringRecord> {
     records
 }
 
-// pub fn get_headers() -> Vec<StringRecord>{
-//
-//     // {
-//     //     let h = reader.headers();
-//     //     println!("{:?}", h);
-//     //     headers.copy(h);
-//     // }
-// }
