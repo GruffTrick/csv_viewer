fd27269c47a1c4b72d611b6a465ba061575f9615
GruffTrick <gruff.trick@gmail.com>
2023-03-09 12:15:19 +0000

    new function calls.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::borrow::{BorrowMut};
 use std::process::exit;
+use std::clone;
 use csv::StringRecord;
 use csv_viewer::*;
 
 
 fn main() {
     let mut reader = get_reader_stdin();
-    let mut headers: Vec<StringRecord> = Vec::new();
-    let mut records = get_records(reader);
 
-    run_app(headers, records).expect("Panic: Empty records");
+    let mut headers: StringRecord = get_headers(reader.borrow_mut());
+
+    let mut records = get_records(reader.borrow_mut());
+
+    run_app(headers, records, None).expect("Panic: Empty records");
 }
