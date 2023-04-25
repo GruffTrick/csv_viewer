254a3186ef59490e93eab4eb3cb08ec3cde34b21
Gruffudd Trick <74840512+GruffTrick@users.noreply.github.com>
2023-04-25 13:37:42 +0100

    template tests

---
+++ tests/test_reader.rs
@@ -0,0 +1,14 @@
+use csv_viewer::reader;
+
+#[cfg(test)]
+mod tests_reader {
+  use super::*;
+  
+  #[test]
+  /// assert the main menu window opens successfully
+  fn test_show_main_menu() {
+    
+        // code here
+    
+  }
+}
