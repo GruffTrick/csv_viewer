98ed2706a8e656a679b7405e572c5c8effccbe15
Gruffudd Trick <74840512+GruffTrick@users.noreply.github.com>
2023-04-25 13:37:32 +0100

    template for tests.

--- tests/test_viewer.rs
+++ tests/test_viewer.rs
@@ -1,2 +1,33 @@
 use csv_viewer::viewer_app;
 use csv_viewer::viewer;
+
+
+#[cfg(test)]
+mod tests_viewer {
+  use super::*;
+  
+  #[test]
+  /// assert equality of read StringRecord from line to expected StringRecord
+  fn test_read_line() {
+    
+        // code here
+    
+  }
+  
+  #[test]
+  // assert equality of actual headers of testfile to the headers read by file.
+  fn test_read_headers() {
+    
+      // code here
+  }
+  
+  #[test]
+  /// assert equality of the actual number of records in file 
+  /// to the row count interpereted by the application.
+  fn test_total_row_count() {
+  
+      // code here
+  }
+  
+  
+}
